2854f2f40916dc142b8c7600f94b0973a1c0680b
Artyom Chernetsov <chernetsov.artyom@gmail.com>
2023-04-27 21:51:23 +0300

    Clean code

--- vue-front/src/stores/keycloakStore.ts
+++ vue-front/src/stores/keycloakStore.ts
@@ -1,10 +1,10 @@
-import { ref } from 'vue'
-import { defineStore } from 'pinia'
 import type Keycloak from "keycloak-js";
+import { defineStore } from 'pinia';
+import { ref } from 'vue';
 
 // https://pinia.vuejs.org/getting-started.html
 export const useKeycloakStore = defineStore('keycloakStore', () => {
-  const keycloak = ref(null as Keycloak | null)
+  const keycloak = ref<Keycloak>()
 
   return { keycloak }
 })
